94db0951d638e596251e359087929f6ee1d27d0a
Your Name <you@example.com>
2024-05-24 03:36:26 +0200

    first push

---
+++ src/a_outils.c
@@ -0,0 +1,81 @@
+#include "pipex.h"
+
+////////////////////////////////////////////////////////
+// add the \n to arg, check if same as gnl(0)
+static int		same_str_n(char *gnl0, char *stop_w)
+{
+	char 	*temp;
+	int		i;
+
+	i = 0;
+	temp = str("%1s\n", stop_w);
+	if (same_str(gnl0, temp))
+		i++;
+	free(temp);
+	return (i);
+}
+
+// dup2 and close fd
+void dup_close(int fd_replace, int fd_erase)
+{
+	if (fd_replace < 0 || fd_erase < 0)
+		return ;
+	dup2(fd_replace, fd_erase);
+    close(fd_replace);
+}
+
+////////////////////////////////////////////////////////
+// add the \n to arg, check if same as gnl(0)
+void	ft_heredoc(char **av, t_pip *pip)
+{
+	int		fd[2];
+	pid_t 	pid;
+	char 	*tmp;
+
+    if (pipe(fd) == -1)
+        return (perror("pipe"));
+    pid = fork();
+    if (pid == -1)
+        return (perror("fork"));
+	if (!pid)
+	{
+		close(fd[0]);
+		while (1)
+		{
+			tmp = gnl(0);
+			if (!tmp || same_str_n(tmp, av[2]))
+				(close(fd[1]), free_s(tmp), exit(0));
+			print_fd(fd[1], "%s", tmp);
+			tmp = free_s(tmp);
+		}
+	}
+	else
+		(close(fd[1]), dup_close(fd[0], STDIN), wait(NULL));
+}
+
+// ////////////////////////////////////////////////////////
+// ./pipex infile cmd1 cmd2 cmd3 outfile = 6
+void ft_child(char *raw_cmd, char **env, t_pip *pip)
+{
+	pid_t	pid;
+	int		fd[2];
+
+	if (pipe(fd) == -1)
+        return (perror("pipe"));
+    pid = fork();
+    if (pid == -1)
+        return (perror("fork"));
+    if (!pid)// 	< child
+    {
+    	close(pip->outfile);
+        close(fd[0]);
+        dup_close(fd[1], STDOUT);
+        ft_exec(raw_cmd, env);
+    }
+    else// 			< parent
+    {
+        close(fd[1]);
+        dup_close(fd[0], STDIN);
+		waitpid(pid, NULL, 0);
+    }
+}
---
+++ src/b_exec.c
@@ -0,0 +1,78 @@
+#include "../include/pipex.h"
+
+static char	**parse_path(char **env)
+{
+	while (*env)
+	{
+		if (*env[0] == 'P' && !find_str(*env, "PATH="))
+			return(split(&(*env)[5], ":"));
+		env++;
+	}
+	return (NULL);
+}
+
+static char	*find_parsing(char *command, char **env)
+{
+	char **paths;
+    char *cmd;
+    int i;
+
+    paths = parse_path(env);
+    if (!paths)
+        return (put(RED"ERROR--->$PATH:\n%t"RESET, env), NULL);
+    cmd = NULL;
+    i = -1;
+    while (paths[++i])
+    {
+        cmd = str("%1s/%1s", paths[i], command);
+        if (!access(cmd, X_OK))
+            break ;
+        cmd = free_s(cmd);
+    }
+    free_tab(paths);
+	return (cmd);
+}
+
+static char *find_cmd(char *command, char **env)
+{
+    char *rtrn;
+
+    rtrn = NULL;
+    if (!command)
+        return (NULL);
+	if (find_str(command, "/") >= 0)
+    {
+        if (access(command, X_OK) == 0)
+            return (str("%1s", command));
+        return (NULL);
+    }
+    rtrn = find_parsing(command, env);
+    return (rtrn);
+}
+
+// raw_cmd = "ls -la -lb"
+void ft_exec(char *raw_cmd, char **env)
+{
+    char **argve;
+    char *cmd;
+
+    argve = split(raw_cmd, " ");
+    if (!argve)
+        return ;
+    cmd = find_cmd(argve[0], env);
+    if (!cmd)
+    {
+        dup2(STDERR_FILENO, 1);
+        put("error: command not found: %s\n", argve[0]);
+        free_tab(argve);
+        exit(1);
+    }
+    if (execve(cmd, argve, env) == -1)
+    {
+        dup2(STDERR_FILENO, 1);
+        perror("error execve");
+        free_tab(argve);
+        free(cmd);
+        exit(1);
+    }
+}
